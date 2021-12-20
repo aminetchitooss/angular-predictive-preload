27f77ab3c839098a6fd7be351fab80c559d3b172
Amine tchita <amine.tchita1@gmail.com>
2021-12-20 16:09:51 +0100

    initial Commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Angular Azure Msal
+
+[![npm](https://img.shields.io/npm/v/@tchitos/angular-predictive-preload.svg?maxAge=2592000?style=flat-square)](https://www.npmjs.com/package/@tchitos/angular-predictive-preload)
+[![npm](https://img.shields.io/npm/dm/@tchitos/angular-predictive-preload.svg)](https://www.npmjs.com/package/@tchitos/angular-predictive-preload)
+
+Angular Predictive Preload - This package supports Angular 13+
+
+## Installation
+
+1. Create a new project
+
+```sh
+ng new test
+cd test
+
+# !! Make sure in the package.json to set "rxjs": "~6.6.0"
+```
+
+2. Include the predictive preload routing strategy into your application.
+
+```sh
+ng add @tchitos/angular-predictive-preload
+
+```
+
+3. Run the application et voila!
+
+```sh
+ng serve
+```
+
+## Test the schematics locally
+
+1. Clone this repository.
+
+```sh
+git clone https://github.com/aminetchitooss/angular-predictive-preload.git
+```
+
+2. Install dependencies
+
+```sh
+npm install
+```
+
+3. Make changes and then run
+
+```bash
+npm run build
+schematics .:ng-add --dry-run=false
+```
+
+## License
+
+MIT
---
+++ src/ng-add/index.ts
@@ -0,0 +1,80 @@
+import { Rule, SchematicContext, Tree, chain, noop, mergeWith, apply, url, template, move } from "@angular-devkit/schematics";
+import { NodePackageInstallTask } from "@angular-devkit/schematics/tasks";
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+export function ngAdd(options: MsalSchematicOption): Rule {
+  return chain([
+    options && options.skipPackageJson ? noop() : addPackageJsonDependency(),
+    updateAppRouting(options),
+
+    mergeWith(apply(url("./files"), [template({}), move(options.appDir)]))
+  ]);
+}
+
+// install dependency to package.json and install
+function addPackageJsonDependency() {
+  return (_host: Tree, _context: SchematicContext) => {
+    if (_host.exists("package.json")) {
+      const jsonStr = _host.read("package.json")!.toString("utf-8");
+      const json = JSON.parse(jsonStr);
+
+      const type = "dependencies";
+      if (!json[type]) {
+        json[type] = {};
+      }
+
+      const globalPkg = "@tchitos/angular-predictive-preload";
+      const version = "latest";
+      if (!json[type][globalPkg]) {
+        json[type][globalPkg] = version;
+      }
+      _context.logger.log("info", "@tchitos/angular-predictive-preload was added as dependency");
+
+      _host.overwrite("package.json", JSON.stringify(json, null, 2));
+
+      _context.addTask(new NodePackageInstallTask());
+    }
+    return _host;
+  };
+}
+
+export function updateAppRouting(options: MsalSchematicOption) {
+  return (_host: Tree, _context: SchematicContext) => {
+    const indexPath = options.appDir + "app-routing.module.ts";
+
+    if (_host.exists(indexPath)) {
+      const content: Buffer | null = _host.read(indexPath);
+      let strContent: string = "";
+      if (content) strContent = content.toString();
+
+      const updatedContent = addPreloadStrategy(strContent);
+
+      _host.overwrite(indexPath, updatedContent);
+    } else {
+      _context.logger.log("error", "Missing File with path => " + indexPath);
+    }
+
+    return _host;
+  };
+}
+
+function addPreloadStrategy(str: string): string {
+  if (str.indexOf("preloadingStrategy") == -1) {
+    str = "import { PredictivePreloadingStrategy } from './app.preloadStrategy';" + str;
+    if (str.indexOf("RouterModule.forRoot(routes,{") > -1) {
+      return str.replace("RouterModule.forRoot(routes,{", "RouterModule.forRoot(routes,{ preloadingStrategy: PredictivePreloadingStrategy,");
+    } else if (str.indexOf("RouterModule.forRoot(routes, {") > -1) {
+      return str.replace("RouterModule.forRoot(routes, {", "RouterModule.forRoot(routes, { preloadingStrategy: PredictivePreloadingStrategy,");
+    }
+  } else if (str.indexOf("RouterModule.forRoot(routes)") > -1) {
+    return str.replace("RouterModule.forRoot(routes", "RouterModule.forRoot(routes, { preloadingStrategy: PredictivePreloadingStrategy}),");
+  }
+  return str;
+}
+
+export interface MsalSchematicOption {
+  srcDir: string;
+  appDir: string;
+  [key: string]: any;
+}
